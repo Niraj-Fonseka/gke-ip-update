bf759244a4a6f0e00bc931833a1138ae9b05a21f
Niraj-Fonseka <fonseka.live@gmail.com>
2020-05-05 23:46:18 -0500

    adding some args

--- gke-ip.go
+++ gke-ip.go
@@ -15,6 +15,7 @@ package main
 //    project directory.
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,23 +26,22 @@ import (
 	"google.golang.org/api/container/v1"
 )
 
+var (
+	CredentialPath *string
+	ProjectID      *string
+	ClusterZone    *string
+	ClusterID      *string
+)
+
 func main() {
+	HandleArgs()
 
-	projectID := os.Getenv("GCP_PROJECT")
+}
 
-	if projectID == "" {
-		log.Fatal("Project is not set")
-	}
-	clusterZone := os.Getenv("GCP_KUBE_MASTER_ZONE")
-
-	if clusterZone == "" {
-		log.Fatal("Cluster Zone is not set")
-	}
-
-	clusterID := os.Getenv("GCP_KUBE_CLUSTER_ID")
-
-	if clusterID == "" {
-		log.Fatal("Cluster ID is not set")
+func SetCreds() {
+	log.Println("Setting Google Credentials ")
+	if err := os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/home/hungryotter/go/src/gke-ip-update/sa.json"); err != nil {
+		log.Fatal(err)
 	}
 
 	ctx := context.Background()
@@ -101,6 +101,30 @@ func main() {
 
 	// TODO: Change code below to process the `resp` object:
 	fmt.Printf("%#v\n", resp)
+}
+func HandleArgs() {
+	CredentialPath = flag.String("path", "", "path for the google application credentials")
+	ProjectID = flag.String("project", "", "project id")
+	ClusterID = flag.String("string", "", "cluster id")
+	ClusterZone = flag.String("zone", "", "zone where the master lives")
+	flag.Parse()
+
+	if *CredentialPath == "" {
+		log.Fatal("No path for the service account provided")
+	}
+
+	if *ProjectID == "" {
+		log.Fatal(("No project provided"))
+	}
+
+	if *ClusterZone == "" {
+		log.Fatal("No zone provided")
+	}
+
+	if *ClusterID == "" {
+		log.Fatal("ClusterID is not provided ")
+	}
+	fmt.Printf("Credentials Path : %s , ProjectID : %s , ClusterZone : %s , ClusteID : %s \n", *CredentialPath, *ProjectID, *ClusterZone, *ClusterID)
 }
 
 //https://cloud.google.com/kubernetes-engine/docs/reference/rest/v1/projects.zones.clusters/get?apix_params=%7B%22projectId%22%3A%22agile-terra-275621%22%2C%22zone%22%3A%22us-central1-c%22%2C%22clusterId%22%3A%22projects-cluster%22%7D
