a255d64ab537d480a77b41a388ef52ee4cb1dbb1
Niraj-Fonseka <fonseka.live@gmail.com>
2020-05-06 01:31:22 -0500

    cleanup

--- gke-ip.go
+++ gke-ip.go
@@ -1,19 +1,5 @@
 package main
 
-// BEFORE RUNNING:
-// ---------------
-// 1. If not already done, enable the Kubernetes Engine API
-//    and check the quota for your project at
-//    https://console.developers.google.com/apis/api/container
-// 2. This sample uses Application Default Credentials for authentication.
-//    If not already done, install the gcloud CLI from
-//    https://cloud.google.com/sdk/ and run
-//    `gcloud beta auth application-default login`.
-//    For more information, see
-//    https://developers.google.com/identity/protocols/application-default-credentials
-// 3. Install and update the Go dependencies by running `go get -u` in the
-//    project directory.
-
 import (
 	"flag"
 	"fmt"
@@ -86,7 +72,6 @@ func initializeLocalStorage() {
 	}
 
 	if _, err := os.Stat(homePath + "/.gke_ip_update"); os.IsNotExist(err) {
-		// path/to/whatever does not exist
 		err := os.Mkdir(homePath+"/.gke_ip_update", 0755)
 		if err != nil {
 			log.Fatal("Unable to create ,.gke_ip_update")
@@ -153,7 +138,7 @@ func setGKEIP(ip, displayName string) {
 		log.Fatal(err)
 	}
 
-	existingBlocks, err := GetExistingCidrBlock(*ProjectID, *ClusterZone, *ClusterID, c, containerService)
+	existingBlocks, err := getExistingCidrBlock(*ProjectID, *ClusterZone, *ClusterID, c, containerService)
 
 	if err != nil {
 		log.Fatal(err)
@@ -187,17 +172,14 @@ func setGKEIP(ip, displayName string) {
 
 	rb := &container.UpdateClusterRequest{
 		Update: &clusterUpdate,
-		// TODO: Add desired fields of the request body. All existing fields
-		// will be replaced.
 	}
 
-	resp, err := containerService.Projects.Zones.Clusters.Update(*ProjectID, *ClusterZone, *ClusterID, rb).Context(ctx).Do()
+	_, err := containerService.Projects.Zones.Clusters.Update(*ProjectID, *ClusterZone, *ClusterID, rb).Context(ctx).Do()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// TODO: Change code below to process the `resp` object:
-	fmt.Printf("%#v\n", resp)
+	log.Println("IP successfully updated in the gke cluster")
 }
 
 func handleArgs() {
@@ -232,7 +214,7 @@ func handleArgs() {
 }
 
 //https://cloud.google.com/kubernetes-engine/docs/reference/rest/v1/projects.zones.clusters/get?apix_params=%7B%22projectId%22%3A%22agile-terra-275621%22%2C%22zone%22%3A%22us-central1-c%22%2C%22clusterId%22%3A%22projects-cluster%22%7D
-func GetExistingCidrBlock(projectID string, zone string, clusterID string, client *http.Client, containerService *container.Service) ([]*container.CidrBlock, error) {
+func getExistingCidrBlock(projectID string, zone string, clusterID string, client *http.Client, containerService *container.Service) ([]*container.CidrBlock, error) {
 	ctx := context.Background()
 	resp, err := containerService.Projects.Zones.Clusters.Get(projectID, zone, clusterID).Context(ctx).Do()
 	if err != nil {
